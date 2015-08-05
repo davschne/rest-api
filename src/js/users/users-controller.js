0da7195f097addb71a40ad607393a19bffb2dc03
davschne <davschne@gmail.com>
2015-08-05 12:33:48 -0700

    AJAX for getAll() and destroy()

--- src/js/users/users-controller.js
+++ src/js/users/users-controller.js
@@ -1,3 +1,8 @@
+function handle(res, msg) {
+  $scope.errors.push({msg: msg});
+  console.log(res.data);
+};
+
 module.exports = function(app) {
   app.controller('usersController', ['$scope', '$http', function($scope, $http) {
     $scope.users = [];
@@ -8,8 +13,16 @@ module.exports = function(app) {
           $scope.users = res.data;
         })
         .catch(function(res) {
-          $scope.errors.push({msg: 'Couldn\'t retrieve users'});
-          console.log(res.data);
+          handle(res, 'Couldn\'t retrieve users');
+        });
+    };
+    $scope.destroy = function(user) {
+      $http.delete('/api/users/' + user._id)
+        .then(function(res) {
+          $scope.users.splice($scope.users.indexOf(user), 1);
+        })
+        .catch(function(res) {
+          handle(res, 'Error deleting user');
         });
     };
   }]);

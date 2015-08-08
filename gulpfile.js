1df3e80739967f460a0ed4844d7ebad52faa5767
davschne <davschne@gmail.com>
2015-08-08 12:31:56 -0700

    Set up test environment - initialized Karma - set up Gulp tasks for client tests - created testing files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var webpack = require('gulp-webpack');
+var KarmaServer = require('karma').Server;
 
 gulp.task('webpack:build', function() {
   return gulp.src('src/js/client.js')
@@ -11,13 +12,30 @@ gulp.task('webpack:build', function() {
     .pipe(gulp.dest('build/'));
 });
 
+gulp.task('webpack:test', function() {
+  return gulp.src('test/client_test/entry.js')
+    .pipe(webpack({
+      output: {
+        filename: 'test_bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('test/client_test/'));
+});
+
 gulp.task('copy', function() {
   return gulp.src('src/**/*(*.html|*.css)')
     .pipe(gulp.dest('build/'));
 });
 
+gulp.task('karma', ['webpack:test'], function(done) {
+  new KarmaServer({
+    configFile: __dirname + '/karma.conf.js'
+  }, done).start();
+});
+
 gulp.task('build', ['webpack:build', 'copy']);
-gulp.task('default', ['build']);
+
+gulp.task('default', ['karma', 'build']);
 
 gulp.task('watch', function() {
   gulp.watch('src/**/*.*', ['default']);
